813239d2760e1955c625f1b8580b38efe85d9c34
VedantSri14 <srivastavavishal.vs@gmail.com>
2020-11-22 17:47:23 +0530

    Using Firebase

--- sketch.js
+++ sketch.js
@@ -1,29 +1,38 @@
 var ball;
+var database, position;
 
-function setup(){
-    createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+function setup() {
+	createCanvas(500, 500);
+	database = firebase.database();
+	ball = createSprite(250, 250, 10, 10);
+	ball.shapeColor = "red";
+	var ballpos = database.ref("ball/position").on("value", readpos, showerror);
+}
+function readpos(data) {
+	position = data.val();
+	ball.x = position.x;
+	ball.y = position.y;
+}
+function showerror() {
+	console.log("There's an error!");
+}
+function draw() {
+	background("white");
+	if (keyDown(LEFT_ARROW)) {
+		changePosition(-1, 0);
+	} else if (keyDown(RIGHT_ARROW)) {
+		changePosition(1, 0);
+	} else if (keyDown(UP_ARROW)) {
+		changePosition(0, -1);
+	} else if (keyDown(DOWN_ARROW)) {
+		changePosition(0, +1);
+	}
+	drawSprites();
 }
 
-function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
-    }
-    drawSprites();
-}
-
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function changePosition(x, y) {
+	database.ref('ball/position').set({
+		x: ball.x + x,
+		y: ball.y + y
+	});
 }
